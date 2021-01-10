36a225cb309fb4a2d08a9134be8f77f2fb764a5c
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2021-01-10 09:48:54 -0300

    Fixed GetSystemPython not working correctly on Windows/get-python.bat

--- source/tools/Windows/get-python.bat
+++ source/tools/Windows/get-python.bat
@@ -4,12 +4,14 @@ REM After the executable is found, executes it if an argument is given, exits if
 set DEBUG_SCRIPT=0
 set PATH_ROOT=.\launcher\
 set PATH_FILE=\python_executable.txt
+set RESOLVED_PATH=None
 
 :GetWindows32Python
 if %DEBUG_SCRIPT%==1 echo GetWindows32Python
 set WIN_VERSION=Windows32
 if exist %PATH_ROOT%%WIN_VERSION%%PATH_FILE% (
     set /p PYTHON_EXECUTABLE=<%PATH_ROOT%%WIN_VERSION%%PATH_FILE%
+    set RESOLVED_PATH=Windows32Python
     goto ResolvePathVariable
 ) else (
     goto GetWindows64Python
@@ -20,6 +22,7 @@ if %DEBUG_SCRIPT%==1 echo GetWindows64Python
 set WIN_VERSION=Windows64
 if exist %PATH_ROOT%%WIN_VERSION%%PATH_FILE% (
     set /p PYTHON_EXECUTABLE=<%PATH_ROOT%%WIN_VERSION%%PATH_FILE%
+    set RESOLVED_PATH=Windows64Python
     goto ResolvePathVariable
 ) else (
     goto GetWindowsPython
@@ -30,6 +33,7 @@ if %DEBUG_SCRIPT%==1 echo GetWindowsPython
 set WIN_VERSION=Windows
 if exist %PATH_ROOT%%WIN_VERSION%%PATH_FILE% (
     set /p PYTHON_EXECUTABLE=<%PATH_ROOT%%WIN_VERSION%%PATH_FILE%
+    set RESOLVED_PATH=WindowsPython
     goto ResolvePathVariable
 ) else (
     goto GetSystemPython
@@ -49,13 +53,13 @@ if %ERRORLEVEL%==0 (
 if %DEBUG_SCRIPT%==1 echo ResolvePathVariable
 set PYTHON_EXECUTABLE=set %PYTHON_EXECUTABLE%
 %PYTHON_EXECUTABLE%
-if exist %PYTHON_EXECUTABLE% (
-    goto CheckArgument
-) else (
-    goto PythonNotFound
-)
+if exist %PYTHON_EXECUTABLE% goto CheckArgument
+if %RESOLVED_PATH% == Windows32Python goto GetWindows64Python
+if %RESOLVED_PATH% == Windows64Python goto GetWindowsPython
+if %RESOLVED_PATH% == WindowsPython goto GetSystemPython
 
 :CheckArgument
+if %DEBUG_SCRIPT%==1 echo CheckArgument
 if "%~1" == "" (
     goto EndOfScript
 ) else (
