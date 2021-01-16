ae0dad07b5bf3a04cd04e8dbddabf7f230936e0f
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2021-01-15 21:49:06 -0300

    Fixed set-icons script due to incompatibility with new 32 and 64 bit engine folders

--- source/tools/Common/helper_scripts/common.py
+++ source/tools/Common/helper_scripts/common.py
@@ -19,17 +19,20 @@ def getData():
     if configFile.exists():
         data["CurPath"] = curPath
         data["Quote"] = '"' if platform.system() == "Windows" else "'"
+        data["EngineExecutables"] = []
         
         with open(configFile.as_posix(), 'r') as sourceFile:
             data.update(literal_eval(sourceFile.read()))
             print("> Read config from", configFile.as_posix())
             
-        engineFile = curPath / ('./launcher/' + platform.system() + '/engine_executable.txt')
+        engineFiles = [p / "engine_executable.txt" for p in (curPath / "launcher").iterdir() if ("Windows" in p.name or "Linux" in p.name) and p.is_dir()]
         
-        if engineFile.exists():
-            with open(engineFile.as_posix(), 'r') as sourceFile:
-                data["EngineExecutable"] = Path(sourceFile.read().split('=')[-1].replace('"', "").replace("'", ""))
-                print("> Read engine path from", engineFile.as_posix())
+        for engineFile in engineFiles:
+            if engineFile.exists():
+                with open(engineFile.as_posix(), 'r') as sourceFile:
+                    engineExecutable = Path(sourceFile.read().split('=')[-1].replace('"', "").replace("'", "").replace("\n", ""))
+                    if engineExecutable.exists():
+                        data["EngineExecutables"].append(engineExecutable.resolve())
                 
     return data
 
--- source/tools/Common/helper_scripts/set_icons.py
+++ source/tools/Common/helper_scripts/set_icons.py
@@ -10,28 +10,36 @@ import common
 data = common.getData()
 
 if data is not None:
-    platExt = ".exe" if platform.system() == "Windows" else ""
-    launcherPath = data["CurPath"] / (common.formatFileName(data["GameName"]) + platExt)
-    launcherPath = data["CurPath"] / (common.formatFileName(data["GameName"]) + platExt)
-    launcherFallbackPath = data["CurPath"] / ("BGArmor" + platExt)
-    enginePath = data["EngineExecutable"]
     
-    if not launcherPath.exists():
-        launcherPath = launcherFallbackPath
-    
-    if launcherPath.exists():
-        command = '?./source/tools/Windows/rcedit.exe? --set-icon ?./source/icons/icon-launcher.ico? '
-        command += '?' + launcherPath.as_posix() + '?'
-        command = command.replace('?', data["Quote"])
-        print("\n> Setting icon of launcher...")
-        print("Command:", command)
-        subprocess.call(command)
+    if platform.system() == "Windows":
+        launcherPath = data["CurPath"] / (common.formatFileName(data["GameName"]) + ".exe")
+        launcherFallbackPath = [f for f in data["CurPath"].iterdir() if f.name.endswith(".exe") and f.is_file()]
+        launcherFallbackPath = launcherFallbackPath[0] if len(launcherFallbackPath) > 0 else data["CurPath"] / ("BGArmor.exe")
+        enginePaths = data["EngineExecutables"]
         
-    if enginePath.exists():
-        command = '?./source/tools/Windows/rcedit.exe? --set-icon ?./source/icons/icon-engine.ico? '
-        command += '?' + enginePath.as_posix() + '?'
-        command = command.replace('?', data["Quote"])
-        print("\n> Setting icon of engine...")
-        print("Command:", command)
-        subprocess.call(command)
+        if not launcherPath.exists():
+            launcherPath = launcherFallbackPath
+        
+        if launcherPath.exists():
+            command = '?./source/tools/Windows/rcedit.exe? --set-icon ?./source/icons/icon-launcher.ico? '
+            command += '?' + launcherPath.as_posix() + '?'
+            command = command.replace('?', data["Quote"])
+            print("\n> Setting icon of launcher...")
+            print("Command:", command)
+            subprocess.call(command)
+            
+        else:
+            print("\nX Launcher executable not found! Try using the name", data["GameName"] + ".exe")
+            
+        for enginePath in enginePaths:
+            if "Windows" in enginePath.parent.name:
+                command = '?./source/tools/Windows/rcedit.exe? --set-icon ?./source/icons/icon-engine.ico? '
+                command += '?' + enginePath.as_posix() + '?'
+                command = command.replace('?', data["Quote"])
+                print("\n> Setting icon of engine...")
+                print("Command:", command)
+                subprocess.call(command)
+                
+    else:
+        print("X Script only available on Windows!")
         
