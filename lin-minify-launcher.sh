6b2cfca91aabe2f4ea1732b5c036bad0a0504e7a
Joel Gomes <joelgomes1994@hotmail.com>
2021-05-10 13:08:25 -0300

    Improved minified launcher script.

--- lin-minify-launcher.sh
+++ lin-minify-launcher.sh
@@ -12,6 +12,7 @@ source './source/tools/Linux/get-python.sh'
 if [ ! -z $PYTHON_EXECUTABLE ]; then
     PYMINIFIER="$PYTHON_EXECUTABLE -m source.tools.Common.pyminifier"
     $PYMINIFIER -o './launcher/launcher.py' --replacement-length=48 --obfuscate-classes --obfuscate-functions --obfuscate-import-methods --obfuscate-builtins './source/launcher.py'
+    $PYTHON_EXECUTABLE './source/tools/Common/helper_scripts/minify_launcher.py'
 fi
 
 echo
