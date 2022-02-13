d2980657810dd719b5a972cbc405db4508ca658f
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2022-02-13 11:20:02 -0300

    feat: implement launcher execution with Python interpreter

--- source/launcher/src/main.rs
+++ source/launcher/src/main.rs
@@ -2,7 +2,7 @@ use std::path::Path;
 use std::env;
 use clap::{Arg, App};
 use std::fs;
-// use subprocess::Popen;
+use subprocess::*;
 
 
 fn main() {
@@ -26,25 +26,24 @@ fn main() {
             .help("Pass arguments to engine executable (in quotes)"))
         .get_matches();
         
+    let executables = get_executables();
     
-    let cur_os = if env::consts::FAMILY == "windows" {"Windows"} else {"Linux"};
-    let executables = get_executables(cur_os);
     if executables.len() == 2 {
-        for el in executables {
-            println!("{}", el);
-        }
+        let mut args: Vec<String> = Vec::new();
+        
         if matches.is_present("a") {
+            args.push("a".to_string());
         }
+        
+        run_python_executable(executables, args);
     } else {
         println!("X Could not find any Python and engine executable")
     }
 }
 
-
-fn get_executables(cur_os: &str) -> Vec<String> {
+fn get_executables() -> Vec<String> {
+    let cur_os = if env::consts::FAMILY == "windows" {"Windows"} else {"Linux"};
     let launcher_base_path_str = String::from("./launcher/");
-    let python_config_name = "python_executable.txt";
-    let engine_config_name = "engine_executable.txt";
     
     let launcher_paths = [
         [launcher_base_path_str.clone().as_str(), cur_os, "32/"].join(""),
@@ -54,16 +53,16 @@ fn get_executables(cur_os: &str) -> Vec<String> {
     for path in launcher_paths {
         let path = String::from(path);
         
-        let python_config_path_str = [path.clone(), python_config_name.to_string()].join("");
-        let engine_config_path_str = [path.clone(), engine_config_name.to_string()].join("");
-        
+        let python_config_path_str = [path.clone(), "python_executable.txt".to_string()].join("");
+        let engine_config_path_str = [path.clone(), "engine_executable.txt".to_string()].join("");
         let python_config_path = Path::new(&python_config_path_str);
         let engine_config_path = Path::new(&engine_config_path_str);
         
         let python_executable_path_str = get_executable_from_config(&python_config_path).clone();
         let engine_executable_path_str = get_executable_from_config(&engine_config_path).clone();
-        let python_executable_path = Path::new(python_executable_path_str.as_str());
-        let engine_executable_path = Path::new(engine_executable_path_str.as_str());
+        let python_executable_path = Path::new(&python_executable_path_str);
+        let engine_executable_path = Path::new(&engine_executable_path_str);
+        
         let mut executables: Vec<String> = Vec::new();
         
         if python_executable_path.exists() {
@@ -100,10 +99,48 @@ fn get_executable_from_config(config_path: &Path) -> String {
             let executable_path = Path::new(executable_path_str.as_str());
             
             if executable_path.exists() {
-                return executable_path_str;
+                return executable_path.canonicalize().unwrap().into_os_string().into_string().unwrap();
             }
         }
         return String::from("");
     }
     return String::from("");
+}
+
+fn run_python_executable(executables: Vec<String>, _args: Vec<String>) {
+    
+    // Enable executable execution on Linux
+    if env::consts::FAMILY != "windows" {
+        
+        for el in executables.iter() {
+            
+            let mut _process = Popen::create(&["chmod", "+x", el.as_str()], PopenConfig {
+                stdout: Redirection::Pipe, ..Default::default()
+            }).expect("X Could not enable execution of Python executable");
+        }
+    }
+    
+    let mut launcher_script = Path::new("./launcher/launcher.py");
+    
+    if !launcher_script.exists() {
+        launcher_script = Path::new("./source/launcher.py");
+        
+        if !launcher_script.exists() {
+            println!("X Could not find script launcher.py");
+            return;
+        }
+    }
+    
+    let launcher_script = launcher_script.canonicalize().unwrap();
+    let launcher_script = launcher_script.as_path();
+    
+    // Execute launcher script on Python interpreter
+    for el in executables.iter() {
+            
+        let mut _process = Popen::create(&[el.as_str(), launcher_script.to_str().unwrap()], PopenConfig {
+            stdout: Redirection::Pipe, ..Default::default()
+        }).expect("X Could not run script launcher.py");
+        
+        return;
+    }
 }
