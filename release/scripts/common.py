4c1010159e1d724e714fd4ae3d5de866c5d9a987
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2022-02-20 11:33:31 -0300

    feat: update common.py - make getProjectData generic - add type comments - create function getArgs

--- release/scripts/common.py
+++ release/scripts/common.py
@@ -1,42 +1,61 @@
-import os
-import platform
-import subprocess
-import string
-from pathlib import Path
-from ast import literal_eval
+from pathlib import Path as _Path
 
-def getData():
-    data = {}
+
+def getArgs():
+    # type: () -> dict[str, str]
     
-    curPath = Path(__file__).parent.resolve()
+    from sys import argv
     
-    if curPath.name == 'helper_scripts':
-        curPath = curPath.parent.parent.parent.parent
-        os.chdir(curPath.as_posix())
+    args = {} # type: dict[str, str]
+    
+    for i in range(len(argv)):
+        if i > 0:
+            arg = argv[i]
+            argNext = argv[i + 1] if len(argv) > i + 1 else True
+            
+            if arg.startswith("-"):
+                args[arg] = argNext
+                
+    return args
+
+
+def getProjectData(projectFile):
+    # type: (str | _Path) -> dict[str, object]
+    
+    import platform
+    from ast import literal_eval
+    
+    data = {} # type: dict[str, object]
+    projectFile = _Path(projectFile).resolve() if type(projectFile) == str else projectFile.resolve() # type: _Path
+    
+    if projectFile.exists():
+        curPath = projectFile.parent
         
-    configFile = curPath / './launcher/config.json'
-    
-    if configFile.exists():
+        with open(projectFile.as_posix(), "r") as sourceFile:
+            data.update(literal_eval(sourceFile.read()))
+            print("> Read config from", projectFile.as_posix())
+            
+        engineExecutables = [] # type: list[_Path]
+        
+        for key in data.keys():
+            if "Engine" in key:
+                engineExecutable = curPath / data[key] # type: _Path
+                
+                if engineExecutable.exists:
+                    engineExecutables.append(engineExecutable)
+                        
         data["CurPath"] = curPath
         data["Quote"] = '"' if platform.system() == "Windows" else "'"
-        data["EngineExecutables"] = []
-        
-        with open(configFile.as_posix(), 'r') as sourceFile:
-            data.update(literal_eval(sourceFile.read()))
-            print("> Read config from", configFile.as_posix())
-            
-        engineFiles = [p / "engine_executable.txt" for p in (curPath / "launcher").iterdir() if ("Windows" in p.name or "Linux" in p.name) and p.is_dir()]
-        
-        for engineFile in engineFiles:
-            if engineFile.exists():
-                with open(engineFile.as_posix(), 'r') as sourceFile:
-                    engineExecutable = Path(sourceFile.read().split('=')[-1].replace('"', "").replace("'", "").replace("\n", ""))
-                    if engineExecutable.exists():
-                        data["EngineExecutables"].append(engineExecutable.resolve())
+        data["EngineExecutables"] = engineExecutables
                 
     return data
 
+
 def formatFileName(name, spaces=True, lowercase=False):
+    # type: (str, bool, bool) -> str
+    
+    import string
+    
     allowedChars = string.ascii_lowercase + string.digits + ".-"
     result = ""
     
@@ -54,3 +73,4 @@ def formatFileName(name, spaces=True, lowercase=False):
         if c in allowedChars:
             result += c
     return result
+
