59fd3e671f89a93c4edf061b33942abeff53ef7a
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2022-03-01 21:10:13 -0300

    feat: launcher executable read and run from config.json

--- source/launcher/src/main.rs
+++ source/launcher/src/main.rs
@@ -1,24 +1,21 @@
-use std::path::Path;
 use std::env;
-use clap::{Arg, App};
 use std::fs;
+use std::path::{PathBuf};
+use clap::{Arg, App};
+use json;
 use subprocess::*;
 
-
 fn main() {
+    let config = get_config();
     
     let matches = App::new("BGArmor Launcher")
-        .version("0.0.6")
+        .version("0.1.0")
         .author("Joel Gomes da Silva <joelgomes1994@hotmail.com>")
         .about("BGE and UPBGE source code obfuscator and launcher toolchain.")
         .arg(Arg::with_name("console")
             .short("c")
             .long("console")
             .help("Enable console window"))
-        .arg(Arg::with_name("log")
-            .short("l")
-            .long("log")
-            .help("Log messages in console"))
         .arg(Arg::with_name("args")
             .short("a")
             .long("args")
@@ -26,50 +23,71 @@ fn main() {
             .help("Pass arguments to engine executable (in quotes)"))
         .get_matches();
         
-    let executables = get_executables();
-    
-    if executables.len() == 2 {
-        let mut args: Vec<String> = Vec::new();
+    if config.has_key("GameName") {
+        let executables = get_executables(&config);
         
-        if matches.is_present("a") {
-            args.push("a".to_string());
+        if executables.len() == 2 {
+            let mut args: Vec<String> = Vec::new();
+            
+            if matches.is_present("a") {
+                args.push("a".to_string());
+            }
+            
+            run_python_executable(executables, args);
+        } else {
+            println!("X Could not find any Python and engine executable")
         }
-        
-        run_python_executable(executables, args);
-    } else {
-        println!("X Could not find any Python and engine executable")
     }
 }
 
-fn get_executables() -> Vec<String> {
+/// Get base directory where launcher executable is located.
+fn get_base_path() -> std::path::PathBuf {
+    let root_path = env::current_exe().unwrap().to_path_buf();
+    let root_path = root_path.parent().unwrap().to_path_buf();
+    return root_path;
+}
+
+/// Read config file from launcher directory and returns it as JsonValue.
+fn get_config() -> json::JsonValue {
+    let mut config_path = get_base_path();
+    config_path.push("launcher/config.json");
+    
+    if config_path.exists() && config_path.is_file() {
+        let config_data_str = fs::read_to_string(config_path).expect("X Could not read file config.json");
+        return json::parse(config_data_str.as_str()).expect("X Could not parse file config.json");
+    }
+    println!("X Could not find config.json");
+    return json::parse("{}").unwrap();
+}
+
+/// Get the first existing Python and engine executables from config as a Vec.
+fn get_executables(config: &json::JsonValue) -> Vec<String> {
+    let launcher_base_path_str: PathBuf = get_base_path();
     let cur_os = if env::consts::FAMILY == "windows" {"Windows"} else {"Linux"};
-    let launcher_base_path_str = String::from("./launcher/");
+    let architectures = ["32", "64"];
     
-    let launcher_paths = [
-        [launcher_base_path_str.clone().as_str(), cur_os, "32/"].join(""),
-        [launcher_base_path_str.clone().as_str(), cur_os, "64/"].join(""),
-    ];
-    
-    for path in launcher_paths {
-        let path = String::from(path);
+    for architecture in architectures {
+        let key_engine = ["Engine", cur_os, architecture].join("");
+        let key_python = ["Python", cur_os, architecture].join("");
         
-        let python_config_path_str = [path.clone(), "python_executable.txt".to_string()].join("");
-        let engine_config_path_str = [path.clone(), "engine_executable.txt".to_string()].join("");
-        let python_config_path = Path::new(&python_config_path_str);
-        let engine_config_path = Path::new(&engine_config_path_str);
-        
-        let python_executable_path_str = get_executable_from_config(&python_config_path).clone();
-        let engine_executable_path_str = get_executable_from_config(&engine_config_path).clone();
-        let python_executable_path = Path::new(&python_executable_path_str);
-        let engine_executable_path = Path::new(&engine_executable_path_str);
+        let path_engine_str = &config[key_engine].as_str().unwrap();
+        let path_python_str = &config[key_python].as_str().unwrap();
+        let mut path_engine = launcher_base_path_str.clone();
+        path_engine.push(path_engine_str);
+        let mut path_python = launcher_base_path_str.clone();
+        path_python.push(path_python_str);
         
         let mut executables: Vec<String> = Vec::new();
-        
-        if python_executable_path.exists() {
-            executables.push(python_executable_path_str.clone());
             
-            if engine_executable_path.exists() {
-                executables.push(engine_executable_path_str.clone());
+        if path_python.is_file() && path_python.exists() {
+            path_python.canonicalize().unwrap();
+            let path_python_str = path_python.to_str().unwrap();
+            executables.push(String::from(path_python_str));
+            
+            if path_engine.is_file() && path_engine.exists() {
+                path_engine.canonicalize().unwrap();
+                let path_engine_str = path_engine.to_str().unwrap();
+                executables.push(String::from(path_engine_str));
                 return executables;
             }
         }
@@ -77,32 +95,7 @@ fn get_executables() -> Vec<String> {
     return Vec::new();
 }
 
-fn get_executable_from_config(config_path: &Path) -> String {
-    
-    if config_path.exists() {
-        let file_data_str = String::from(fs::read_to_string(config_path).expect("").as_str());
-        let mut elements: Vec<String> = Vec::new();
-        
-        for el in file_data_str.split("=") {
-            let mut el = String::from(el.clone());
-            el = String::from(el.trim());
-            el = el.replace("'", "").replace("\"", "");
-            elements.push(el);
-        }
-        
-        if elements.len() == 2 {
-            let executable_path_str = elements[1].clone();
-            let executable_path = Path::new(executable_path_str.as_str());
-            
-            if executable_path.exists() {
-                return executable_path.canonicalize().unwrap().into_os_string().into_string().unwrap();
-            }
-        }
-        return String::from("");
-    }
-    return String::from("");
-}
-
+// Run the launcher script in the Python executable.
 fn run_python_executable(executables: Vec<String>, _args: Vec<String>) {
     
     // Enable executable execution on Linux
@@ -125,19 +118,15 @@ fn run_python_executable(executables: Vec<String>, _args: Vec<String>) {
         }
     }
     
-    let mut launcher_script = Path::new("./launcher/launcher.py");
+    let mut launcher_script = get_base_path();
+    launcher_script.push("launcher/launcher.py");
     
     if !launcher_script.exists() {
-        launcher_script = Path::new("./source/launcher.py");
-        
-        if !launcher_script.exists() {
-            println!("X Could not find script launcher.py");
-            return;
-        }
+        println!("X Could not find script launcher.py");
+        return;
     }
     
     let launcher_script = launcher_script.canonicalize().unwrap();
-    let launcher_script = launcher_script.as_path();
     
     // Execute launcher script on Python interpreter
     for el in executables.iter() {
