25996c7d429f1000f144383abe18569f0bcfdb8b
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2022-03-27 16:47:51 -0300

    feat: replace makefile with python script - available commands: minify, build_windows, build_linux

---
+++ build.py
@@ -0,0 +1,69 @@
+import sys
+import os
+import subprocess
+import shutil
+from pathlib import Path
+
+
+curDir = Path(__file__).parent
+pythonExecutable = Path(sys.executable).resolve()
+
+
+def main():
+    # type: () -> None
+    
+    os.chdir(curDir.as_posix())
+    
+    if sys.argv[-1] in operators.keys():
+        operators[sys.argv[-1]]()
+    
+    else:
+        print("X Invalid command:", sys.argv[-1])
+        print("Available commands:", ", ".join([i for i in operators.keys()]))
+
+
+def build(target):
+    # type: (str) -> None
+    
+    toolchain = "i686-pc-windows-gnu" if target == "windows" else "i686-unknown-linux-gnu"
+    ext = ".exe" if target == "windows" else ""
+    launcherSourcePath = (curDir / "source/launcher")
+    sourceExe = launcherSourcePath / ("target/{toolchain}/release/bgarmor{ext}".format(toolchain=toolchain, ext=ext))
+    targetExe = curDir / ("release/launcher/Launcher" + ext)
+    
+    print("> Started build for target:", toolchain)
+    
+    os.chdir(launcherSourcePath.as_posix())
+    
+    if targetExe.exists():
+        print("> Deleted existing executable:", targetExe.as_posix())
+        targetExe.unlink()
+        
+    args = ["cargo", "build", "--target=" + toolchain, "--release"]
+    print("> Running cargo build:", " ".join(args))
+    subprocess.call(args)
+    
+    print("> Copying executable to:", targetExe.as_posix())
+    shutil.copy2(sourceExe.as_posix(), targetExe.as_posix())
+    
+    if target != "windows":
+        args = ["upx", "-5", targetExe.as_posix()]
+        print("> Running upx on executable:", " ".join(args))
+        subprocess.call(args)
+        
+    print("> Done!")
+
+
+def minify():
+    # type: () -> None
+    script = curDir / "source/scripts/minify_launcher.py"
+    subprocess.call([pythonExecutable.as_posix(), script.as_posix()])
+
+
+operators = {
+    "build_windows": lambda: build("windows"),
+    "build_linux": lambda: build("linux"),
+    "minify": minify,
+}  # type: dict[str, object]
+
+main()
