ca7fc2c9d6d1f84a34a2edaa80b8f70befed1c1e
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2022-06-17 22:36:55 -0300

    feat: add support to set icons on linux using hacker with wine

--- release/scripts/set_icons.py
+++ release/scripts/set_icons.py
@@ -1,5 +1,6 @@
 from pathlib import Path as _Path
 import common as _common
+import platform
 
 
 args = _common.getArgs()
@@ -18,54 +19,58 @@ def main():
 
         if args.get("--resource-hacker"):
 
-            if platform.system() == "Windows":
-                launcherPath = data["CurPath"] / "launcher/Launcher.exe"  # type: _Path
-                enginePaths = data["EngineExecutables"]  # type: dict[str, _Path]
+            launcherPath = data["CurPath"] / "launcher/Launcher.exe"  # type: _Path
+            enginePaths = data["EngineExecutables"]  # type: dict[str, _Path]
 
-                if launcherPath.exists():
-                    launcherIconPath = data["CurPath"] / "icons/icon-launcher.ico"  # type: _Path
-                    command = _getResourceHackerCommand(launcherPath, launcherIconPath)
-                    print("\n> Setting icon of launcher...")
-                    print("Command:", command)
-                    subprocess.call(command)
-
-                else:
-                    print("\nX Launcher executable not found! Try using the name", _common.formatFileName(data["GameName"]) + ".exe")
-
-                for enginePath in enginePaths.values():
-
-                    if "Windows" in enginePath.parent.name:
-                        engineIconPath = data["CurPath"] / "icons/icon-engine.ico"  # type: _Path
-                        command = _getResourceHackerCommand(enginePath, engineIconPath)
-                        print("\n> Setting icon of engine...")
-                        print("Command:", command)
-                        subprocess.call(command)
-
-                print("> Done!")
+            if launcherPath.exists():
+                launcherIconPath = data["CurPath"] / "icons/icon-launcher.ico"  # type: _Path
+                command = _getResourceHackerCommand(launcherPath, launcherIconPath)
+                print("\n> Setting icon of launcher...")
+                print("Command:", " ".join(command))
+                subprocess.call(command)
 
             else:
-                print("X Command only available on Windows")
+                print("\nX Launcher executable not found! Try using the name", _common.formatFileName(data["GameName"]) + ".exe")
+
+            for enginePath in enginePaths.values():
+
+                if "Windows" in enginePath.parent.name:
+                    engineIconPath = data["CurPath"] / "icons/icon-engine.ico"  # type: _Path
+                    command = _getResourceHackerCommand(enginePath, engineIconPath)
+                    print("\n> Setting icon of engine...")
+                    print("Command:", " ".join(command))
+                    subprocess.call(command)
+
+            print("> Done!")
 
         else:
             print("X Argument --resource-hacker must be provided")
 
 
 def _getResourceHackerCommand(executablePath, iconPath):
-    # type: (_Path, _Path) -> str
+    # type: (_Path, _Path) -> list[str]
 
     resourceHackerPath = _Path(args.get("--resource-hacker"))
+    useWine = platform.system() != "Windows"
 
     if resourceHackerPath.exists():
         resourceHackerPath = resourceHackerPath.resolve()
 
-    command = '?' + resourceHackerPath.as_posix() + '? '
-    command += '-open ?' + executablePath.as_posix() + '? '
-    command += '-save ?' + executablePath.as_posix() + '? '
-    command += '-action addoverwrite -res ?' + iconPath.as_posix() + '? '
-    command += '-mask ICONGROUP,APPICON,'
-    command = command.replace('?', data["Quote"])
+        command = ['wine'] if useWine else []
+        command += [quote(resourceHackerPath.as_posix())]
+        command += ['-open', quote(executablePath.as_posix())]
+        command += ['-save', quote(executablePath.as_posix())]
+        command += ['-action', 'addoverwrite', '-res', quote(iconPath.as_posix())]
+        command += ['-mask', 'ICONGROUP,APPICON,']
 
-    return command
+        return command
+
+    return []
+
+
+def quote(value, placeholder='?'):
+    # type: (str, str) -> str
+    return value.replace(placeholder, data["Quote"])
 
 
 try:
